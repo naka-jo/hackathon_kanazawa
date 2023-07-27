46b33e5363413616ed8a605a7dfd2f555661dfe7
naka-jo <s2222027@stu.musashino-u.ac.jp>
2023-07-27 19:01:39 +0900

    クーポンデータベース一体化

--- classes/coupondb.py
+++ classes/coupondb.py
@@ -1,41 +1,42 @@
 import sqlite3
 
-def create_table(dbname):
-    conn = sqlite3.connect(f'../database/{dbname}')
+def create_table(table):
+    conn = sqlite3.connect(f'../database/coupon.db')
     cur = conn.cursor()
     cur.execute(
-        'CREATE TABLE IF NOT EXISTS coupon_table(id INTEGER PRIMARY KEY, data TEXT, discount TEXT, store TEXT, category TEXT, remarks TEXT);'
+        f'CREATE TABLE IF NOT EXISTS {table}(id INTEGER PRIMARY KEY, data TEXT, discount TEXT, store TEXT, category TEXT, remarks TEXT);'
     )
     conn.commit()
     conn.close()
 
-def insert_db(dbname, data):
-    conn = sqlite3.connect(f'../database/{dbname}')
+def insert_db(table, data):
+    conn = sqlite3.connect(f'../database/coupon.db')
     cur = conn.cursor()
-    cur.execute('INSERT INTO coupon_table(id, data, discount, store, category, remarks) values(?, ?, ?, ?, ?, ?);', data)
+    cur.execute(f'INSERT INTO {table}(id, data, discount, store, category, remarks) values(?, ?, ?, ?, ?, ?);', data)
     conn.commit()
     conn.close()
-    
-def select_db(dbname):
-    conn = sqlite3.connect(f'../database/{dbname}')
+
+def select_db(table):
+    conn = sqlite3.connect(f'../database/coupon.db')
     cur = conn.cursor()
-    cur.execute('SELECT * FROM coupon_table;')
+    cur.execute(f'SELECT * FROM {table};')
     result = cur.fetchall()
     conn.close()
     return result
 
-def delete_column(dbname, id):
-    conn = sqlite3.connect(f'../database/{dbname}')
+def delete_table(table, id):
+    conn = sqlite3.connect(f'../database/coupon.db')
     cur = conn.cursor()
-    cur.execute(f'DELETE FROM coupon_table WHERE id={id};')
+    cur.execute(f'DELETE FROM {table} WHERE id={id};')
     conn.commit()
     conn.close()
 
-# dbname = 'hiroyuki'
-# create_table(dbname)
+# dbname = 'coupon'
+# table = 'kanra'
+# create_table(dbname, table)
 # data = [None, '7/30', '50円引き', 'なか卯', '飲食', None]
 # data = [None, '8/3', '10%off', 'スターバックス', '飲食', None]
-# insert_db(dbname, data)
-# delete_column(dbname, 1)
-# result = select_db(dbname)
+# insert_db(dbname, table, data)
+# delete_table(dbname, table, 3)
+# result = select_db(dbname, table)
 # print(result)
