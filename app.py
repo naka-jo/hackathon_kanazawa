3f2d2c6e0c4bd77a51420e150e3f3fd4a23ccda3
pdmuds4 <s2222015@stu.musashino-u.ac.jp>
2023-07-28 04:11:19 +0900

    edit some files

--- app.py
+++ app.py
@@ -1,51 +1,55 @@
 from flask import Flask, render_template, request, redirect, url_for, flash
 from classes.userdb import UserDB
-import uuid
+import classes.coupondb as CouponDB
+import uuid, json
 
 app = Flask(__name__, static_folder="./static")
 app.config["SECRET_KEY"] = str(uuid.uuid4().hex)
-UserDB().reset()
+#UserDB().reset()
 
 @app.route("/login", methods=["GET", "POST"])
 def login(): # ログイン画面
-    if request.method == "GET":
-      return render_template("login.html")
-    else:
-      email = request.form["email"]
-      password = request.form["password"] # get email & password
-      user = UserDB(email, password)
-      user_id = user.getid_byemail()
+  if request.method == "GET":
+    return render_template("login.html")
+  else:
+    email = request.form["email"]
+    password = request.form["password"] # get email & password
+    user = UserDB(email, password)
+    user_id = user.getid_byemail()
 
-      if user.logincheck():
-        return redirect(url_for("home", id=user_id)) # 登録成功 -> idのurlに飛ぶ
-      else:
-        flash("入力が正しくありません")
-        return redirect(url_for("login"))
+    if user.logincheck():
+      return redirect(url_for("home", id=user_id)) # 登録成功 -> idのurlに飛ぶ
+    else:
+      flash("入力が正しくありません")
+      return redirect(url_for("login"))
 
 
 
 @app.route("/account", methods=["GET", "POST"])
 def account(): # アカウント登録
-    if request.method == "GET":
-      return render_template("account.html")
-    else:
-      email = request.form["email"]
-      password = request.form["password"] # get email & password
-      user = UserDB(email, password)
+  if request.method == "GET":
+    return render_template("account.html")
+  else:
+    email = request.form["email"]
+    password = request.form["password"] # get email & password
+    user = UserDB(email, password)
 
-      if user.emailcheck(): # 有効なemailかcheck
-        user.insert()
-        return redirect(url_for("home", id=user.id)) # 登録成功 -> DB登録
-      else:
-        flash('このメールアドレスは既に使われているか、使用できません')
-        return redirect(url_for("account"))
+    if user.emailcheck(): # 有効なemailかcheck
+      user.insert()
+      CouponDB.create_table(user.id)
+      return redirect(url_for("home", id=user.id)) # 登録成功 -> DB登録
+    else:
+      flash('このメールアドレスは既に使われているか、使用できません')
+      return redirect(url_for("account"))
 
 
 
 @app.route("/home/<string:id>", methods=["GET", "POST"])
 def home(id):
     if request.method == "GET":
-      # test
+      user_data = json.loads(CouponDB.select_db(id))
+      return render_template("home.html", user_data=user_data)
+    else:
       return render_template("home.html")
 
 
