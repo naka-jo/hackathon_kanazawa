82c8e630edaa6386c9e2118eec5750429743e3b0
naka-jo <s2222027@stu.musashino-u.ac.jp>
2023-07-29 10:01:53 +0900

    データ検索機能を追加、パスのミスを修正

--- classes/coupondb.py
+++ classes/coupondb.py
@@ -3,7 +3,7 @@ import json
 import os
 
 def create_table(table): # テーブルを作成
-    conn = sqlite3.connect(f"./database/coupon.db")
+    conn = sqlite3.connect("../database/coupon.db")
     cur = conn.cursor()
     cur.execute(
         f"CREATE TABLE IF NOT EXISTS {table}(id INTEGER PRIMARY KEY, data TEXT, discount TEXT, store TEXT, category TEXT, remarks TEXT);"
@@ -12,14 +12,14 @@ def create_table(table): # テーブルを作成
     conn.close()
 
 def insert_db(table, data): # データを追加
-    conn = sqlite3.connect(f"./database/coupon.db")
+    conn = sqlite3.connect("../database/coupon.db")
     cur = conn.cursor()
     cur.execute(f"INSERT INTO {table}(id, data, discount, store, category, remarks) values(?, ?, ?, ?, ?, ?);", data)
     conn.commit()
     conn.close()
 
 def select_db(table): # データを取得
-    conn = sqlite3.connect(f"./database/coupon.db")
+    conn = sqlite3.connect("../database/coupon.db")
     cur = conn.cursor()
     cur.execute(f"SELECT * FROM {table};")
     dict_values = cur.fetchall()
@@ -31,8 +31,21 @@ def select_db(table): # データを取得
         result.append(result_dict)
     return json.dumps(result)
 
+def search_db(table, word):
+    conn = sqlite3.connect("../database/coupon.db")
+    cur = conn.cursor()
+    cur.execute(f"SELECT * FROM {table} WHERE category = '{word}';")
+    dict_values = cur.fetchall()
+    conn.close()
+    dict_keys = ("id", "date", "discount", "store", "category", "remarks")
+    result = []
+    for values in dict_values:
+        result_dict = {k: v for k, v in zip(dict_keys, values)}
+        result.append(result_dict)
+    return json.dumps(result)
+
 def delete_table(table, id): # データを消去
-    conn = sqlite3.connect(f"./database/coupon.db")
+    conn = sqlite3.connect("../database/coupon.db")
     cur = conn.cursor()
     cur.execute(f"DELETE FROM {table} WHERE id={id};")
     conn.commit()
